86e5900cc695bc96c9919e0bce1c2cc19fb35d93
Julien Reichardt <mi@jrei.ch>
2019-08-09 00:35:47 +0200

    Use Crystal::Parser

--- src/object_send.cr
+++ src/object_send.cr
@@ -1,65 +1,46 @@
+require "compiler/crystal/syntax"
+
 class Object
-  def send(call : String)
-    send call.strip
+  private def cast_node(node : Crystal::ASTNode)
+    case node
+    when Crystal::StringLiteral then node.value
+    when Crystal::CharLiteral   then node.value
+    when Crystal::BoolLiteral   then node.value
+    when Crystal::NilLiteral    then nil
+    when Crystal::NumberLiteral
+      case node.kind
+      when :f32 then node.value.to_f32
+      when :f64 then node.value.to_f64
+      else           node.integer_value
+      end
+    else
+      raise "unsupported node type: #{node} (#{node.class}}"
+    end
   end
 
-  macro send(call)
-    {% supported_types = %w(String Int Char Nil Float64) %}
-    {% arg_count = [1, 2, 3, 4] %}
-    {% for arg_num in arg_count %}\
-    arg{{arg_num.id}} = nil
-    {% end %}
-    arg_number = 0
+  def send(call : String)
+    {% begin %}
+    {% supported_types = %w(Int Bool Char Float32 Float64 Int16 Int32 Int64 Int8 String UInt16 UInt32 UInt64 UInt8 Nil) %}
 
-    cast = -> (string : String) {
-      if string.starts_with?('"') && string.ends_with?('"')
-        string.lchop.rchop
-      elsif int32 = string.to_i?
-        int32
-      elsif float64 = string.to_f?
-        float64
-      elsif (string.size == 3) && string.starts_with?('\'') && string.ends_with?('\'')
-        string[1]
-      else
-        raise "unsupported type: " + string
+    case node = Crystal::Parser.parse(call)
+    when Crystal::Call
+      raise "max number of arguments reached: 8" if node.args.size > 9
+      if obj = node.obj
+        node.args << obj
       end
-    }
-
-    if {{call.id}}.ends_with? ')'
-      method, str_args = {{call.id}}.rchop(')').split('(', limit: 2)
-    elsif {{call.id}}.includes? ' '
-      method, str_args = {{call.id}}.split(' ', limit: 2)
+      {% args = %w(0 1 2 3 4 5 6 7 8 9) %}
+      method_with_args = case node.args.size
+      {% for arg_num in args %}\
+      when {{arg_num.id}}
+        { node.name {% for local_arg_num in args %}{% if local_arg_num < arg_num %}, cast_node(node.args[{{local_arg_num.id}}]){% end %}{% end %} }
+      {% end %}
+      end
     else
-      method = {{call.id}}
-    end
-      
-    if str_args
-      if str_args.includes?(',')
-        str_args.split(',') do |val|
-          arg_number += 1
-          case arg_number
-          {% for arg_num in arg_count %}
-          when {{arg_num.id}} then arg{{arg_num.id}} = cast.call val.strip
-          {% end %}
-          else        raise "the current max number of arguments is {{arg_count.last.id}}: " + val
-          end
-        end
-      elsif !str_args.empty?
-        arg_number = 1
-        arg1 = cast.call str_args
-      end
-    end
-    
-    method_with_args = case arg_number
-    when 0 then { method }
-    when 1 then { method, arg1 }
-    when 2 then { method, arg1, arg2 }
-    when 3 then { method, arg1, arg2, arg3 }
-    when 4 then { method, arg1, arg2, arg3, arg4 }
-    else       raise "max number of arguments reached: {{arg_count.last.id}}"
+      raise "unsupported call: #{node}"
     end
 
     case method_with_args
+    when nil then raise "method with args should not be nil for #{call}"
     {% methods = @type.methods %}\
     {% for type in @type.ancestors %}\
       {% methods = methods + type.methods %}\
@@ -81,7 +62,7 @@ class Object
           self.{{method.name}}(
           {% for arg in method.args %}\
             {% i = i + 1 %}\
-            arg{{i.id}}.as({{arg.restriction}}),
+            method_with_args[{{i.id}}]?.as({{arg.restriction}}),
           {% end %}\
           )
         {% used_methods[method.name.stringify + method_args] = true %}\
@@ -90,5 +71,6 @@ class Object
     else
       raise "unsupported method: #{method_with_args}"
     end
+    {% end %}
   end
 end
